050f0eec00ad3503d2832356f3bd36ccc27f84af
SethGamer1223 <155033256+SethGamer1223@users.noreply.github.com>
2026-02-23 17:54:05 -0500

    Update install.lua

--- install.lua
+++ install.lua
@@ -2,7 +2,7 @@ print("Downloading KTWSL..")
 shell.run("wget https://raw.githubusercontent.com/MasonGulu/msks/main/ktwsl.lua ktwsl.lua")
 
 print("Downloading abstractInvLib..")
-shell.run("wget https://gist.githubusercontent.com/MasonGulu/57ef0f52a93304a17a9eaea21f431de6/raw/0e4bba991532b019873f659f09f09f23a0493b4d/abstractInvLib.lua abstractInvLib.lua")
+shell.run("wget https://gist.githubusercontent.com/ShreksHellraiser/57ef0f52a93304a17a9eaea21f431de6/raw/305debf81c2a06f65a035b37c7bc1fb628dc992f/abstractInvLib.lua abstractInvLib.lua")
 
 print("Downloading msks")
 shell.run("wget https://raw.githubusercontent.com/MasonGulu/msks/main/msks.lua msks.lua")
